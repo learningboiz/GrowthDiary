45bf1c4c8a68c6fcc35d3eb784ec662d3c6a9384
Elijah <elijah.quiazon@gmail.com>
2023-08-22 16:03:48 +1000

    created Skill model

---
+++ src/main/java/com/growthdiary/sessionlog/skill/Skill.java
@@ -0,0 +1,26 @@
+package com.growthdiary.sessionlog.skill;
+
+public class Skill {
+    private long id;
+    private String topic;
+    private String category;
+
+    public Skill() {
+    }
+
+    public void setTopic(String topic) {
+        this.topic = topic;
+    }
+
+    public String getTopic() {
+        return this.topic;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getCategory() {
+        return this.category;
+    }
+}
