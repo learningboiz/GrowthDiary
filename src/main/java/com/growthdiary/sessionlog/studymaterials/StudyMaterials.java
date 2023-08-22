b62aee4f292ac466664a71f144ffa20b8f3faa07
Elijah <elijah.quiazon@gmail.com>
2023-08-22 17:38:16 +1000

    created studymaterials model

---
+++ src/main/java/com/growthdiary/sessionlog/studymaterials/StudyMaterials.java
@@ -0,0 +1,16 @@
+package com.growthdiary.sessionlog.studymaterials;
+
+public class StudyMaterials {
+
+    private String materials;
+
+    public StudyMaterials() {
+    }
+
+    public void setMaterials(String materials) {
+        this.materials = materials;
+    }
+    public String getMaterials() {
+        return this.materials;
+    }
+}
