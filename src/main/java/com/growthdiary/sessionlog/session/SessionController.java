b3a302801b8e29f53298053363e4d74fa5fd3d30
Elijah <elijah.quiazon@gmail.com>
2023-08-24 18:32:02 +1000

    set-up controllers for respective services

---
+++ src/main/java/com/growthdiary/sessionlog/session/SessionController.java
@@ -0,0 +1,44 @@
+package com.growthdiary.sessionlog.session;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@RestController
+public class SessionController {
+
+    private final SessionService sessionService;
+
+    @Autowired
+    public SessionController(SessionService sessionService) {
+        this.sessionService = sessionService;
+    }
+
+    @PostMapping("/session")
+    public ResponseEntity<Session> session(@RequestParam LocalDate date,
+                                           @RequestParam LocalTime startTime,
+                                           @RequestParam LocalTime endTime,
+                                           @RequestParam String topic,
+                                           @RequestParam String category,
+                                           @RequestParam Integer rating,
+                                           @RequestParam String distraction,
+                                           @RequestParam String emotion)
+    {
+        Session session = sessionService.createSession(date,
+                startTime,
+                endTime,
+                topic,
+                category,
+                rating,
+                distraction,
+                emotion);
+
+        return new ResponseEntity<>(session, HttpStatus.CREATED);
+    }
+}
