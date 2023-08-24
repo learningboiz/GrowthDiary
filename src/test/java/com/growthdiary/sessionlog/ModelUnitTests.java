8689f8386407537661adda80723a76b7ca358a4f
Elijah <elijah.quiazon@gmail.com>
2023-08-24 11:01:56 +1000

    added simple tests for sessionsSet under skill model

--- src/test/java/com/growthdiary/sessionlog/ModelUnitTests.java
+++ src/test/java/com/growthdiary/sessionlog/ModelUnitTests.java
@@ -5,6 +5,7 @@ import com.growthdiary.sessionlog.session.Session;
 import com.growthdiary.sessionlog.skill.Skill;
 
 import java.time.*;
+import java.util.*;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
@@ -71,6 +72,31 @@ public class ModelUnitTests {
 
         assertEquals(testSkill, test.getSkill());
         assertEquals(testFeedback, test.getFeedback());
+    }
+
+    @Test
+    public void testSkillSession() {
+        Skill testSkill = new Skill();
+        testSkill.setTopic("Java");
+        testSkill.setCategory("Backend Development");
+
+        LocalDate testDate = LocalDate.now();
+
+        Random randNum = new Random();
+
+        int max = 500;
+        for (int i = 0; i < max; i++) {
+            LocalDate newDate = testDate.plusDays(1);
+
+            Session testSession = new Session();
+            testSession.setSkill(testSkill);
+            testSession.setDate(newDate);
+
+            testSkill.addSession(testSession);
+        }
+
+        int size = testSkill.getSessions().size();
+        assertEquals(max, size);
     }
 
 }
