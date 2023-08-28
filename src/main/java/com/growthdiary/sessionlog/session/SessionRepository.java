35688926c717400b5f06848195f9e0c11e3b64f2
Elijah <elijah.quiazon@gmail.com>
2023-08-28 21:57:01 +1000

    updated repo to extend jpa to have both CRUD and pagination behaviours

--- src/main/java/com/growthdiary/sessionlog/session/SessionRepository.java
+++ src/main/java/com/growthdiary/sessionlog/session/SessionRepository.java
@@ -1,9 +1,10 @@
 package com.growthdiary.sessionlog.session;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+/** Extended JpaRepository as it provides methods for both CRUD and pagination */
 @Repository
-public interface SessionRepository extends CrudRepository<Session, Long> {
+public interface SessionRepository extends JpaRepository<Session, Long> {
 }
 
