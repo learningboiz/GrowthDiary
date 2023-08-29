60b511bcd8e4401ed21542b060b4d8677e51b932
Elijah <elijah.quiazon@gmail.com>
2023-08-29 17:41:12 +1000

    created service function to display custom items for history log use case

--- src/main/java/com/growthdiary/sessionlog/sessionhistory/SessionHistoryService.java
+++ src/main/java/com/growthdiary/sessionlog/sessionhistory/SessionHistoryService.java
@@ -18,11 +18,19 @@ public class SessionHistoryService {
         this.sessionRepository = sessionRepository;
     }
 
+
+    /** The default view will present the first 10 sessions of the user's log */
     public Page<Session> defaultDisplay() {
-        int startPage = 0;
-        int defaultDisplay = 10;
-        String defaultArrangement = "startDate";
-        Pageable firstPage = PageRequest.of(startPage, defaultDisplay, Sort.by(defaultArrangement));
-        return sessionRepository.findAll(firstPage);
+        int currentPage = 0;
+        int numItems = 10;
+        String initialSort = "startDate";
+        Pageable defaultPage = PageRequest.of(currentPage, numItems, Sort.by(initialSort));
+        return sessionRepository.findAll(defaultPage);
+    }
+
+    /** The custom view will present 10, 20 or 30 sessions of the user's log */
+    public Page<Session> customDisplay(int currentPage, int numItems) {
+        Pageable customPage = PageRequest.of(currentPage, numItems);
+        return sessionRepository.findAll(customPage);
     }
 }
