03d085bfd23112751df8f265c2dc70b592375799
Elijah <elijah.quiazon@gmail.com>
2023-08-29 17:42:44 +1000

    set-up controller mapping for custom history that adheres to RESTful API principles

--- src/main/java/com/growthdiary/sessionlog/sessionhistory/SessionHistoryController.java
+++ src/main/java/com/growthdiary/sessionlog/sessionhistory/SessionHistoryController.java
@@ -6,21 +6,30 @@ import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SessionHistoryController {
 
-    private final SessionHistoryService sessionLogService;
+    private final SessionHistoryService sessionHistoryService;
 
     @Autowired
-    public SessionHistoryController(SessionHistoryService sessionLogService) {
-        this.sessionLogService = sessionLogService;
+    public SessionHistoryController(SessionHistoryService sessionHistoryService) {
+        this.sessionHistoryService = sessionHistoryService;
     }
 
     @GetMapping("/history")
-    public ResponseEntity<Page<Session>> history(){
-        Page<Session> history = sessionLogService.defaultDisplay();
-        return new ResponseEntity<>(history, HttpStatus.ACCEPTED);
+    public ResponseEntity<Page<Session>> defaultHistoryDisplay(){
+        Page<Session> defaultDisplay = sessionHistoryService.defaultDisplay();
+        return new ResponseEntity<>(defaultDisplay, HttpStatus.ACCEPTED);
+    }
+
+    @GetMapping("/history/custom")
+    public ResponseEntity<Page<Session>> customHistoryDisplay(@RequestParam int currentPage,
+                                                              @RequestParam int numItems){
+        Page<Session> customDisplay = sessionHistoryService.customDisplay(currentPage, numItems);
+        return  new ResponseEntity<>(customDisplay, HttpStatus.ACCEPTED);
     }
 }
