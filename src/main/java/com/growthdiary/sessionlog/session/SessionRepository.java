99d8a7cee9b960abbea5dbba6dff35114b949290
Elijah <elijah.quiazon@gmail.com>
2023-08-29 16:54:11 +1000

    updated comment for JpaRepo usage

--- src/main/java/com/growthdiary/sessionlog/session/SessionRepository.java
+++ src/main/java/com/growthdiary/sessionlog/session/SessionRepository.java
@@ -3,7 +3,7 @@ package com.growthdiary.sessionlog.session;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-/** Extended JpaRepository as it provides methods for both CRUD and pagination */
+/** Extended JpaRepository as it provides CRUD and Pagination functionalities */
 @Repository
 public interface SessionRepository extends JpaRepository<Session, Long> {
 }
