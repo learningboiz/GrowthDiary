206fab743ad50beacf349769039a4d6f8682f834
Elijah <elijah.quiazon@gmail.com>
2023-08-31 00:20:47 +1000

    added validation to the historyDisplay method

--- src/main/java/com/growthdiary/sessionlog/sessionhistory/SessionHistoryController.java
+++ src/main/java/com/growthdiary/sessionlog/sessionhistory/SessionHistoryController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -21,15 +22,10 @@ public class SessionHistoryController {
     }
 
     @GetMapping("/history")
-    public ResponseEntity<Page<Session>> defaultHistoryDisplay(){
-        Page<Session> defaultDisplay = sessionHistoryService.defaultDisplay();
-        return new ResponseEntity<>(defaultDisplay, HttpStatus.ACCEPTED);
-    }
-
-    @GetMapping("/history/custom")
-    public ResponseEntity<Page<Session>> customHistoryDisplay(@RequestParam int currentPage,
-                                                              @RequestParam int numItems){
-        Page<Session> customDisplay = sessionHistoryService.customDisplay(currentPage, numItems);
+    @Validated
+    public ResponseEntity<Page<Session>> customHistoryDisplay(@RequestParam(defaultValue = "0") int currentPage,
+                                                              @RequestParam(defaultValue = "5") int numItems){
+        Page<Session> customDisplay = sessionHistoryService.getSessionHistory(currentPage, numItems);
         return  new ResponseEntity<>(customDisplay, HttpStatus.ACCEPTED);
     }
 }
