f3eb7ad9be53096713fb6bbb7099fdc2b5b4b47a
Elijah <elijah.quiazon@gmail.com>
2023-09-01 14:05:57 +1000

    modified controller to take in additional parameters for sorting feature

--- src/main/java/com/growthdiary/sessionlog/sessionhistory/SessionHistoryController.java
+++ src/main/java/com/growthdiary/sessionlog/sessionhistory/SessionHistoryController.java
@@ -3,11 +3,11 @@ package com.growthdiary.sessionlog.sessionhistory;
 import com.growthdiary.sessionlog.session.Session;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,9 +23,13 @@ public class SessionHistoryController {
 
     @GetMapping("/history")
     @Validated
-    public ResponseEntity<Page<Session>> customHistoryDisplay(@RequestParam(defaultValue = "0") int currentPage,
-                                                              @RequestParam(defaultValue = "5") int numItems){
-        Page<Session> customDisplay = sessionHistoryService.getSessionHistory(currentPage, numItems);
-        return  new ResponseEntity<>(customDisplay, HttpStatus.ACCEPTED);
+    public ResponseEntity<Page<Session>> historyDisplay(@RequestParam(defaultValue = "0") int currentPage,
+                                                        @RequestParam(defaultValue = "5") int numItems,
+                                                        @RequestParam(defaultValue = "ASC") SortDirection sortDirection,
+                                                        @RequestParam(defaultValue = "startPeriod") String sortProperty)
+    {
+        Sort sort = sessionHistoryService.createSort(sortDirection, sortProperty);
+        Page<Session> customDisplay = sessionHistoryService.getSessionHistory(currentPage, numItems, sort);
+        return new ResponseEntity<>(customDisplay, HttpStatus.ACCEPTED);
     }
 }
