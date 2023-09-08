63ee3409e9a548443b1c1c0d287f59a6cef0c95c
Elijah <elijah.quiazon@gmail.com>
2023-09-08 13:28:07 +1000

    designed and passed unit tests for sessionTime model

--- src/test/java/com/growthdiary/sessionlog/SessionLogUnitTests.java
+++ src/test/java/com/growthdiary/sessionlog/SessionLogUnitTests.java
@@ -2,7 +2,14 @@ package com.growthdiary.sessionlog;
 
 import com.growthdiary.sessionlog.feedback.Feedback;
 import com.growthdiary.sessionlog.session.Session;
+import com.growthdiary.sessionlog.sessiontime.SessionTime;
 import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.temporal.ChronoUnit;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class SessionLogUnitTests {
@@ -11,8 +18,29 @@ public class SessionLogUnitTests {
 
     }
 
+    @Test
     public void timeSanityTest() {
+        LocalDate firstDate = LocalDate.now();
+        LocalDate lastDate = LocalDate.now().plusDays(1);
 
+        LocalTime startTime = LocalTime.now();
+        LocalTime endTime = LocalTime.now().plusMinutes(40);
+
+        SessionTime test = new SessionTime();
+        test.setStartDate(firstDate);
+        test.setEndDate(lastDate);
+        test.setStartTime(startTime);
+        test.setEndTime(endTime);
+
+
+        Long duration = test.calculateDuration();
+        test.setDuration(duration);
+
+        assertEquals(firstDate, test.getFirstDate());
+        assertEquals(lastDate, test.getLastDate());
+        assertEquals(startTime, test.getStartTime());
+        assertEquals(endTime, test.getEndTime());
+        assertEquals(duration, test.getDuration());
     }
 
     @Test
@@ -26,7 +54,5 @@ public class SessionLogUnitTests {
 
         assertEquals(rating, test.getProductivity());
         assertEquals(distraction, test.getDistraction());
-
-
     }
 }
