b7a80254c26c93d516159be92c687a922d16f3d0
Elijah <elijah.quiazon@gmail.com>
2023-09-18 19:38:46 +1000

    refactored Session service methods as per changes made to Time entity

--- src/main/java/com/growthdiary/sessionlog/session/SessionService.java
+++ src/main/java/com/growthdiary/sessionlog/session/SessionService.java
@@ -1,7 +1,7 @@
 package com.growthdiary.sessionlog.session;
 
 import com.growthdiary.sessionlog.feedback.Feedback;
-import com.growthdiary.sessionlog.sessiontime.SessionTime;
+import com.growthdiary.sessionlog.time.Time;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -24,19 +24,19 @@ public class SessionService {
      * Takes user input to create a Session object to be saved into the database
      * @param skill The name of the skill user is working on
      * @param description Specifics of how user is learning that skill
-     * @param sessionTime Start, end and duration of the session
+     * @param time Start, end and duration of the session
      * @param feedback Feedback for the learning session
      * @return Session object
      */
     public Session createSession(String skill,
                                  String description,
-                                 SessionTime sessionTime,
+                                 Time time,
                                  Feedback feedback) {
 
         Session session = new Session();
         session.setSkill(skill);
         session.setDescription(description);
-        session.setSessionTime(sessionTime);
+        session.setSessionTime(time);
         session.setFeedback(feedback);
 
         sessionRepository.save(session);
