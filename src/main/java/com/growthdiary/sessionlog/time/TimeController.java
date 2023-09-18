0afdf31d6f30de735e2198ed14510838abe0c62a
Elijah <elijah.quiazon@gmail.com>
2023-09-18 18:56:12 +1000

    added /session endpoint mapping to TimeController

--- src/main/java/com/growthdiary/sessionlog/time/TimeController.java
+++ src/main/java/com/growthdiary/sessionlog/time/TimeController.java
@@ -1,12 +1,10 @@
 package com.growthdiary.sessionlog.time;
 
-import com.growthdiary.sessionlog.feedback.Feedback;
-import com.growthdiary.sessionlog.session.Session;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,6 +12,7 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 
 @RestController
+@RequestMapping("/session")
 public class TimeController {
 
     private final TimeService timeService;
