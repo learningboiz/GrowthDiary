1fce107830453492da53567108cbd6e4edfd024e
Elijah <elijah.quiazon@gmail.com>
2023-09-19 14:38:26 +1000

    cleaned up code for feedback controller

--- src/main/java/com/growthdiary/sessionlog/feedback/FeedbackController.java
+++ src/main/java/com/growthdiary/sessionlog/feedback/FeedbackController.java
@@ -1,7 +1,5 @@
 package com.growthdiary.sessionlog.feedback;
 
-import com.growthdiary.sessionlog.time.Time;
-import com.growthdiary.sessionlog.time.TimeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,9 +7,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.time.LocalDate;
-import java.time.LocalTime;
 
 @RestController
 @RequestMapping("/session")
@@ -30,7 +25,7 @@ public class FeedbackController {
      */
     @PostMapping("/feedback")
     public ResponseEntity<Feedback> feedback(@RequestParam Integer productivity,
-                                          @RequestParam String distraction) {
+                                             @RequestParam String distraction) {
         Feedback feedback = feedbackService.createFeedback(productivity, distraction);
         return new ResponseEntity<>(feedback, HttpStatus.CREATED);
     }
