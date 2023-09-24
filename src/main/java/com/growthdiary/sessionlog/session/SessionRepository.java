82bb286e563742bc09125e947a73cb147159dd61
Elijah <elijah.quiazon@gmail.com>
2023-09-24 20:50:19 +1000

    removed JpaSpecificationExecutor as that responsibility is handled by the HistoryRepository instead

--- src/main/java/com/growthdiary/sessionlog/session/SessionRepository.java
+++ src/main/java/com/growthdiary/sessionlog/session/SessionRepository.java
@@ -1,11 +1,9 @@
 package com.growthdiary.sessionlog.session;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 /** Extended JpaRepository as it provides CRUD and Pagination functionalities */
 @Repository
-public interface SessionRepository extends JpaRepository<Session, Long>, JpaSpecificationExecutor<Session> {
+public interface SessionRepository extends JpaRepository<Session, Long> {
 }
-
