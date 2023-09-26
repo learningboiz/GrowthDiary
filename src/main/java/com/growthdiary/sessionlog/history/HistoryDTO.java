bd8c953a0b2d8ed4a2950a31f54484978ab15f53
Elijah <elijah.quiazon@gmail.com>
2023-09-27 01:52:00 +1000

    created HistoryDTO to manage arguments required for history services

---
+++ src/main/java/com/growthdiary/sessionlog/history/HistoryDTO.java
@@ -0,0 +1,39 @@
+package com.growthdiary.sessionlog.history;
+
+import org.springframework.data.domain.Sort;
+
+import java.util.List;
+
+public class HistoryDTO {
+
+    private final List<FilterRequest> filterRequest;
+
+    private final int pageNum;
+
+    private final int pageSize;
+
+    private final Sort sort;
+
+    public HistoryDTO(List<FilterRequest> filterRequest, int pageNum, int pageSize, Sort sort) {
+        this.filterRequest = filterRequest;
+        this.pageNum = pageNum;
+        this.pageSize = pageSize;
+        this.sort = sort;
+    }
+
+    public List<FilterRequest> getFilterRequest() {
+        return this.filterRequest;
+    }
+
+    public int getPageNum() {
+        return this.pageNum;
+    }
+
+    public int getPageSize() {
+        return this.pageSize;
+    }
+
+    public Sort getSort() {
+        return this.sort;
+    }
+}
