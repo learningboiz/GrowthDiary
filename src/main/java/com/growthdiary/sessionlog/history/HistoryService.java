3df587f92c75f018bf45bef9519b0ce29bed24a4
Elijah <elijah.quiazon@gmail.com>
2023-09-29 16:00:39 +1000

    Updated HistoryService class: - Created a default view method to be used in the GET controller method (when users first visit the endpoint/history page) - Modified naming convention for private filter method for clarity - Added javadocs to document the usage of each service method

--- src/main/java/com/growthdiary/sessionlog/history/HistoryService.java
+++ src/main/java/com/growthdiary/sessionlog/history/HistoryService.java
@@ -1,12 +1,11 @@
 package com.growthdiary.sessionlog.history;
 
-import com.growthdiary.sessionlog.history.specifications.*;
+import com.growthdiary.sessionlog.history.filters.*;
 import com.growthdiary.sessionlog.tracker.session.Session;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
@@ -22,7 +21,25 @@ public class HistoryService {
         this.historyRepository = historyRepository;
     }
 
-    public Page<Session> getSessions(HistoryDTO historyDTO) {
+    /**
+     * Creates a Page of sessions based on the provided default values
+     * @return a Page of sessions
+     */
+    public Page<Session> getDefaultSessions(){
+
+        int defaultPageNum = 0;
+        int defaultPageSize = 10;
+
+        Pageable pageable = PageRequest.of(defaultPageNum, defaultPageSize);
+        return historyRepository.findAll(pageable);
+    }
+
+    /**
+     * Creates a Page of session based on the user requested parameters
+     * @param historyDTO Object containing user request regarding page numbers, page size, sorts and data filters
+     * @return a Page of filtered session
+     */
+    public Page<Session> getRequestedSessions(HistoryDTO historyDTO) {
 
         Pageable pageable = PageRequest.of(historyDTO.getPageNum(), historyDTO.getPageSize(), historyDTO.getSort());
 
@@ -30,15 +47,11 @@ public class HistoryService {
             return historyRepository.findAll(pageable);
         } else {
             List<FilterRequest> filterRequest = historyDTO.getFilterRequest();
-            return historyRepository.findAll(dynamicFilter(filterRequest), pageable);
+            return historyRepository.findAll(getFilteredSessions(filterRequest), pageable);
         }
     }
 
-    public List<Session> getAllSessions() {
-        return historyRepository.findAll();
-    }
-
-    private Specification<Session> dynamicFilter(List<FilterRequest> filterRequests) {
+    private Specification<Session> getFilteredSessions(List<FilterRequest> filterRequests) {
 
         Specification<Session> compiledSpecs = Specification.where(null);
 
@@ -47,15 +60,15 @@ public class HistoryService {
 
             switch (entity) {
                 case "details": {
-                    Specification<Session> detailsSpec = DetailsSpecifications.createDetailsSpec(filter);
+                    Specification<Session> detailsSpec = DetailsFilter.buildWith(filter);
                     compiledSpecs = compiledSpecs.and(detailsSpec);
                 }
                 case "time": {
-                    Specification<Session> timeSpec = TimeSpecifications.createTimeSpec(filter);
+                    Specification<Session> timeSpec = TimeFilter.buildWith(filter);
                     compiledSpecs = compiledSpecs.and(timeSpec);
                 }
                 case "feedback": {
-                    Specification<Session> feedbackSpec = FeedbackSpecifications.createFeedbackSpec(filter);
+                    Specification<Session> feedbackSpec = FeedbackFilter.buildWith(filter);
                     compiledSpecs = compiledSpecs.and(feedbackSpec);
                 }
             }
