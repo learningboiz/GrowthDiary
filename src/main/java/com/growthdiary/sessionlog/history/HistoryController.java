062059b2e24663d18efcd3af75d4a0310c53f687
Elijah <elijah.quiazon@gmail.com>
2023-10-05 21:01:50 +1100

    added HistoryController method to manage GET requests

--- src/main/java/com/growthdiary/sessionlog/history/HistoryController.java
+++ src/main/java/com/growthdiary/sessionlog/history/HistoryController.java
@@ -1,6 +1,11 @@
 package com.growthdiary.sessionlog.history;
 
+import com.growthdiary.sessionlog.tracker.session.Session;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,4 +18,8 @@ public class HistoryController {
         this.sessionHistoryService = sessionHistoryService;
     }
 
+    @GetMapping("/session/history")
+    public ResponseEntity<Page<Session>> allSessionHistory() {
+        return new ResponseEntity<>(sessionHistoryService.getDefaultSessions(), HttpStatus.ACCEPTED);
+    }
 }
