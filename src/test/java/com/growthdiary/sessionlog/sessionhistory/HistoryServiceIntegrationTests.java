1737c228efbb61b1989f2c0486628bad128040b5
Elijah <elijah.quiazon@gmail.com>
2023-10-11 01:09:58 +1100

    updated tests to reflect changes to the FilterRequest

--- src/test/java/com/growthdiary/sessionlog/sessionhistory/HistoryServiceIntegrationTests.java
+++ src/test/java/com/growthdiary/sessionlog/sessionhistory/HistoryServiceIntegrationTests.java
@@ -1,11 +1,13 @@
 package com.growthdiary.sessionlog.sessionhistory;
 
-import com.growthdiary.sessionlog.history.filters.FilterOperators;
+import com.growthdiary.sessionlog.history.filters.FilterOperations;
 import com.growthdiary.sessionlog.history.HistoryDTO;
 import com.growthdiary.sessionlog.history.HistoryService;
-import com.growthdiary.sessionlog.history.filters.FilterRequest;
+import com.growthdiary.sessionlog.history.requests.FilterRequest;
+import com.growthdiary.sessionlog.history.requests.SortRequest;
 import com.growthdiary.sessionlog.tracker.session.Session;
 import com.growthdiary.sessionlog.tracker.session.SessionRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -13,14 +15,16 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.Specification;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @SpringBootTest
-public class ServiceIntegrationTests {
+public class HistoryServiceIntegrationTests {
 
     @Autowired
     private SessionRepository sessionRepository;
@@ -28,89 +32,27 @@ public class ServiceIntegrationTests {
     @Autowired
     private HistoryService historyService;
 
-    private Sort createSort() {
-        return Sort.by("time.startDate").descending();
-    }
+    private Sort sortRequest;
+    private FilterRequest filterRequest;
 
-    private FilterRequest createDetailsFilter() {
-        String entity = "details";
-        String property = "skill";
-        String skillA = "Spring Boot";
-        String skillB = "TypeScript";
+    @BeforeEach
+    public void setRequestValues() {
+        sortRequest = SortRequest.sortByDescending("time.startDate");
 
-        List<String> skillList = new ArrayList<>();
-        skillList.add(skillA);
-        skillList.add(skillB);
-
-        FilterRequest filter = new FilterRequest.Builder()
-                .entity(entity)
-                .operator(FilterOperators.IN)
-                .property(property)
-                .skills(skillList)
+        filterRequest = new FilterRequest.BuildRequest()
+                .filterSkill(Arrays.asList("Spring Boot", "TypeScript"))
+                .filterDuration(30L, 90L, FilterOperations.BETWEEN)
+                .filterProductivity(2, 3, FilterOperations.BETWEEN)
                 .build();
-
-        return filter;
     }
 
-    private FilterRequest createTimeFilter() {
-        String entity = "time";
-        String property = "duration";
-        Long durationA = 30L;
-        Long durationB = 90L;
-        List<Long> durationList = new ArrayList<>();
-        durationList.add(durationA);
-        durationList.add(durationB);
-
-        FilterRequest filter = new FilterRequest.Builder()
-                .entity(entity)
-                .property(property)
-                .operator(FilterOperators.BETWEEN)
-                .durations(durationList)
-                .build();
-
-        return filter;
-    }
-
-    private FilterRequest createFeedbackFilter() {
-        String entity = "feedback";
-        String property = "productivity";
-        int ratingA = 2;
-        int ratingB = 4;
-        List<Integer> productivityList = new ArrayList<>();
-        productivityList.add(ratingA);
-        productivityList.add(ratingB);
-
-        FilterRequest filter = new FilterRequest.Builder()
-                .entity(entity)
-                .property(property)
-                .operator(FilterOperators.GREATER_THAN)
-                .productivity(productivityList)
-                .build();
-
-        return filter;
-    }
-
-    private List<FilterRequest> createFilterList() {
-        FilterRequest detailsFilter = createDetailsFilter();
-        FilterRequest timeFilter = createTimeFilter();
-        FilterRequest feedbackFilter = createFeedbackFilter();
-
-        List<FilterRequest> filters = new ArrayList<>();
-        filters.add(detailsFilter);
-        filters.add(timeFilter);
-        filters.add(feedbackFilter);
-
-        return filters;
-    }
 
     @Test
     public void testHistoryServiceWithFilter() {
-        List<FilterRequest> filterRequests = createFilterList();
-        Sort sortRequest = createSort();
         int pageNum = 0;
         int pageSize = 10;
 
-        HistoryDTO historyDTO = new HistoryDTO(filterRequests, pageNum, pageSize, sortRequest);
+        HistoryDTO historyDTO = new HistoryDTO(filterRequest, pageNum, pageSize, sortRequest);
 
         Page<Session> sessions = historyService.getRequestedSessions(historyDTO);
         for (Session session : sessions) {
@@ -120,7 +62,6 @@ public class ServiceIntegrationTests {
 
     @Test
     public void testHistoryServiceNoFilter() {
-        Sort sortRequest = createSort();
         int pageNum = 0;
         int pageSize = 10;
         Pageable pageable = PageRequest.of(pageNum, pageSize, sortRequest);
