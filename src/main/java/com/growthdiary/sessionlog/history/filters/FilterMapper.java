32612d11dc3b264c0719ce6d3d3b2489563cf54a
Elijah <elijah.quiazon@gmail.com>
2023-10-11 01:07:34 +1100

    updated FilterMapper as per refactored changes to FilterRequest

--- src/main/java/com/growthdiary/sessionlog/history/filters/FilterMapper.java
+++ src/main/java/com/growthdiary/sessionlog/history/filters/FilterMapper.java
@@ -1,111 +1,116 @@
 package com.growthdiary.sessionlog.history.filters;
 
+import com.growthdiary.sessionlog.history.requests.DetailsRequest;
+import com.growthdiary.sessionlog.history.requests.FeedbackRequest;
 import com.growthdiary.sessionlog.history.requests.FilterRequest;
+import com.growthdiary.sessionlog.history.requests.TimeRequest;
 import com.growthdiary.sessionlog.tracker.session.Session;
 import org.springframework.data.jpa.domain.Specification;
 
-import java.time.LocalDate;
-import java.util.List;
-
 public class FilterMapper {
 
-    /**
-     * Creates a Specification object that combines multiple specifications for each filter
-     * @param filterRequests List containing filter requests
-     * @return a Specification object
-     */
-    public static Specification<Session> getFilteredSessions(List<FilterRequest> filterRequests) {
+    public static Specification<Session> getFilteredSessions(FilterRequest filterRequest) {
 
-        /*
-        Create a base/wrapper specification object to combine other specifications
-         */
-        Specification<Session> compiledSpecs = Specification.where(null);
+        Specification<Session> allSpecifications = Specification.where(null);
 
-        /*
-        Iterate through the list of filter requests
-        Each filter is mapped to the appropriate helper function based on the given entity
-        Exception thrown if the entity provided does not match the existing models
-         */
-        for (FilterRequest request : filterRequests) {
-
-            String entity = request.getEntity();
-
-            switch (entity) {
-                case "details" -> {
-                    Specification<Session> detailsSpec = filterByDetails(request);
-                    compiledSpecs = compiledSpecs.and(detailsSpec);
-                }
-                case "time" -> {
-                    Specification<Session> timeSpec = filterByTime(request);
-                    compiledSpecs = compiledSpecs.and(timeSpec);
-                }
-                case "feedback" -> {
-                    Specification<Session> feedbackSpec = filterByFeedback(request);
-                    compiledSpecs = compiledSpecs.and(feedbackSpec);
-                }
-                default -> throw new IllegalArgumentException("Entity must be 'details', 'time' or 'feedback'");
-            }
+        if (filterRequest.getDetailsRequest() != null) {
+            allSpecifications = allSpecifications.and(filterByDetails(filterRequest.getDetailsRequest()));
         }
-        return compiledSpecs;
+
+        if (filterRequest.getTimeRequest() != null) {
+            allSpecifications = allSpecifications.and(filterByTime(filterRequest.getTimeRequest()));
+        }
+
+        if (filterRequest.getFeedbackRequest() != null) {
+            allSpecifications = allSpecifications.and(filterByFeedback(filterRequest.getFeedbackRequest()));
+        }
+        return null;
     }
 
-    private static Specification<Session> filterByDetails(FilterRequest filterRequest) {
+    private static Specification<Session> filterByDetails(DetailsRequest detailsRequest) {
 
-        Specification<Session> filteredSessions = Specification.where(null);
+        Specification<Session> detailsSpecs = Specification.where(null);
 
-        String entity = filterRequest.getEntity();
-        String property = filterRequest.getProperty();
+        if (detailsRequest.getSkills() != null) {
+            detailsSpecs = detailsSpecs.and(
+                    FilterSpecifications.findValueIn(
+                    detailsRequest.getSkills(),
+                    "details",
+                    "skill"
+                    )
+            );
+        }
 
-        if (property.equals("skill")) {
-            List<String> skills = filterRequest.getSkills();
-            filteredSessions = FilterSpecifications.findValueIn(skills, entity, property);
+        if (detailsRequest.getDescription() != null) {
+            detailsSpecs = detailsSpecs.and(
+                    FilterSpecifications.findValuesLike(
+                            detailsRequest.getDescription(),
+                            "details",
+                            "skill"
+                    )
+            );
+        }
 
-        } else if (property.equals("description")) {
-            String firstValue = filterRequest.getDescriptions().get(0);
-            filteredSessions = FilterSpecifications.findValuesLike(firstValue, entity, property);
-
-        } else throw new RuntimeException("Property provided must be either 'skill' or 'description'");
-
-        return filteredSessions;
+        return detailsSpecs;
     }
 
-    private static Specification<Session> filterByTime(FilterRequest filterRequest) {
+    private static Specification<Session> filterByTime(TimeRequest timeRequest) {
 
-        Specification<Session> filteredSessions = Specification.where(null);
-        String entity = filterRequest.getEntity();
-        String property = filterRequest.getProperty();
+        Specification<Session> timeSpecs = Specification.where(null);
 
-        if (property.equals("duration")) {
-            Long firstValue = filterRequest.getDurations().get(0);
-            Long secondValue = filterRequest.getDurations().get(1);
-            FilterOperations operation = filterRequest.getOperator();
+        if (timeRequest.getDateOp() != null) {
+            timeSpecs = timeSpecs.and(
+                    FilterSpecifications.compareValues(
+                            timeRequest.getDateOp(),
+                            timeRequest.getMinDate(),
+                            timeRequest.getMaxDate(),
+                            "time",
+                            "startDate"
+                    )
+            );
+        }
 
-            filteredSessions = FilterSpecifications.compareValues(operation, firstValue, secondValue, entity, property);
-        } else if (property.equals("startDate")) {
-            LocalDate firstValue = filterRequest.getDates().get(0);
-            LocalDate secondValue = filterRequest.getDates().get(1);
-            FilterOperations operation = filterRequest.getOperator();
+        if (timeRequest.getDurationOp() != null) {
+            timeSpecs = timeSpecs.and(
+                    FilterSpecifications.compareValues(
+                            timeRequest.getDurationOp(),
+                            timeRequest.getMinDuration(),
+                            timeRequest.getMaxDuration(),
+                            "time",
+                            "duration"
+                    )
+            );
+        }
 
-            filteredSessions = FilterSpecifications.compareValues(operation, firstValue, secondValue, entity, property);
-        } else throw new RuntimeException("Property provided must be either 'duration' or 'startDate'");
-        return filteredSessions;
+        return timeSpecs;
     }
 
-    private static Specification<Session> filterByFeedback(FilterRequest filterRequest) {
-        Specification<Session> filteredSessions = Specification.where(null);
-        String entity = filterRequest.getEntity();
-        String property = filterRequest.getProperty();
+    private static Specification<Session> filterByFeedback(FeedbackRequest feedbackRequest) {
 
-        if (property.equals("productivity")) {
-            int firstValue = filterRequest.getProductivity().get(0);
-            int secondValue = filterRequest.getProductivity().get(1);
-            FilterOperations operation = filterRequest.getOperator();
+        Specification<Session> feedbackSpecs = Specification.where(null);
 
-            filteredSessions = FilterSpecifications.compareValues(operation, firstValue, secondValue, entity, property);
-        } else if (property.equals("distraction")) {
-            List<String> distractions = filterRequest.getDistractions();
-            filteredSessions = FilterSpecifications.findValueIn(distractions, entity, property);
-        } else throw new RuntimeException("Property provided must be either 'productivity' or 'distraction'");
-        return filteredSessions;
+        if (feedbackRequest.getProdOp() != null) {
+            feedbackSpecs = feedbackSpecs.and(
+                    FilterSpecifications.compareValues(
+                            feedbackRequest.getProdOp(),
+                            feedbackRequest.getMinProd(),
+                            feedbackRequest.getMaxProd(),
+                            "feedback",
+                            "productivity"
+                    )
+            );
+        }
+
+        if (feedbackRequest.getDistractions() != null) {
+            feedbackSpecs = feedbackSpecs.and(
+                    FilterSpecifications.findValueIn(
+                            feedbackRequest.getDistractions(),
+                            "feedback",
+                            "distractions"
+                    )
+            );
+        }
+
+        return feedbackSpecs;
     }
 }
