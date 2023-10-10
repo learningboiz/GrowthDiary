544701bbc4304bbdede1bc513771c949ac5ca1d7
Elijah <elijah.quiazon@gmail.com>
2023-10-11 01:06:14 +1100

    Refactored FilterRequest class: - Changed design such that a single object will host all the requested filters 	- Removed the need to iterate over multiple filter objects (previous iteration) 	- Protected program from having to process duplicated/multiple same filters (previous iteration) 	- Latest design helps to streamline, simplify and add clarify to the filter creation process - Incorporated validations to ensure arguments passed through were valid from the get-go - Clients can include all 6 filters at one go or do a couple - Empty requests will return an error to reduce wasted object creation - Added javadoc to explain the class and the builder method

--- src/main/java/com/growthdiary/sessionlog/history/requests/FilterRequest.java
+++ src/main/java/com/growthdiary/sessionlog/history/requests/FilterRequest.java
@@ -5,145 +5,130 @@ import com.growthdiary.sessionlog.history.filters.FilterOperations;
 import java.time.LocalDate;
 import java.util.List;
 
+/**
+ * The {@code FilterRequest} class represents an object containing customised requests for viewing session history.
+ * It uses the builder pattern to create customised filter requests
+ * <p>
+ * Use the {@link com.growthdiary.sessionlog.history.requests.FilterRequest.BuildRequest} class to configure a filter request
+ */
 public class FilterRequest {
 
-    private String entity;
+    private DetailsRequest detailsRequest;
 
-    private FilterOperations operator;
+    private TimeRequest timeRequest;
 
-    private String property;
+    private FeedbackRequest feedbackRequest;
 
-    private List<String> skills;
+    private FilterRequest() {
+    }
 
-    private List<String> description;
+    public DetailsRequest getDetailsRequest() {
+        return this.detailsRequest;
+    }
 
-    private List<Long> durations;
+    public TimeRequest getTimeRequest() {
+        return this.timeRequest;
+    }
 
-    private List<LocalDate> dates;
+    public FeedbackRequest getFeedbackRequest() {
+        return this.feedbackRequest;
+    }
 
-    private List<Integer> productivity;
-
-    private List<String> distractions;
-
-    private FilterRequest() {}
-
-    /*
-    Builder method for the FilterRequest object
-    Builder pattern used to support the creation of objects with varying list types
+    /**
+     * The {@code BuildRequest} class provides a builder interface for creating a {@code FilterRequest}.
+     * Use this builder to set various filtering criteria.
      */
-    public static class Builder {
+    public static class BuildRequest {
+
         private final FilterRequest request = new FilterRequest();
+        private DetailsRequest detailsRequest;
 
-        public Builder filterBySkill(List<String> skills) {
-            if (request.entity != null) {
-                throw new UnsupportedOperationException("A maximum of one filter can be applied per request");
-            } else {
-                request.entity = "details";
-                request.property = "skill";
-                request.skills = skills;
-                return this;
-            }
+        private TimeRequest timeRequest;
+
+        private FeedbackRequest feedbackRequest;
+        private boolean filtersApplied = false;
+
+        private void markFilterApplied() {
+            this.filtersApplied = true;
         }
 
-        public Builder filterByDescription(List<String> description) {
-            if (request.entity != null) {
-                throw new UnsupportedOperationException("A maximum of one filter can be applied per request");
-            } else {
-                request.entity = "details";
-                request.property = "description";
-                request.description = description;
-                return this;
+        public BuildRequest filterSkill(List<String> skills) {
+            if (detailsRequest == null) {
+                detailsRequest = new DetailsRequest();
             }
+            detailsRequest.setSkills(skills);
+            markFilterApplied();
+            return this;
         }
 
-        public Builder filterByDuration(List<Long> durations, FilterOperations operation) {
-            if (request.entity != null) {
-                throw new UnsupportedOperationException("A maximum of one filter can be applied per request");
-            } else {
-                request.entity = "time";
-                request.property = "duration";
-                request.operator = operation;
-                request.durations = durations;
-                return this;
+        public BuildRequest filterDescription(String description) {
+            if (detailsRequest == null) {
+                detailsRequest = new DetailsRequest();
             }
+            detailsRequest.setDescription(description);
+            markFilterApplied();
+            return this;
         }
 
-        public Builder filterByDate(List<LocalDate> dates, FilterOperations operation) {
-            if (request.entity != null) {
-                throw new UnsupportedOperationException("A maximum of one filter can be applied per request");
-            } else {
-                request.entity = "time";
-                request.property = "startDate";
-                request.operator = operation;
-                request.dates = dates;
-                return this;
+        public BuildRequest filterDate(LocalDate minDate, LocalDate maxDate, FilterOperations dateOp) {
+            RequestValidator.validateDate(minDate, maxDate, dateOp);
+
+            if (timeRequest == null) {
+                timeRequest = new TimeRequest();
             }
+            timeRequest.setMinDate(minDate);
+            timeRequest.setMaxDate(maxDate);
+            timeRequest.setDateOp(dateOp);
+            markFilterApplied();
+            return this;
         }
 
-        public Builder filterByProductivity(List<Integer> productivity, FilterOperations operation) {
-            if (request.entity != null) {
-                throw new UnsupportedOperationException("A maximum of one filter can be applied per request");
-            } else {
-                request.entity = "feedback";
-                request.property = "productivity";
-                request.operator = operation;
-                request.productivity = productivity;
-                return this;
+        public BuildRequest filterDuration(Long minDuration, Long maxDuration, FilterOperations durationOp) {
+            RequestValidator.validateDuration(minDuration, maxDuration, durationOp);
+
+            if (timeRequest == null) {
+                timeRequest = new TimeRequest();
             }
+
+            timeRequest.setMinDuration(minDuration);
+            timeRequest.setMaxDuration(maxDuration);
+            timeRequest.setDurationOp(durationOp);
+            markFilterApplied();
+            return this;
         }
 
-        public Builder filterByDistraction(List<String> distraction) {
-            if (request.entity != null) {
-                throw new UnsupportedOperationException("A maximum of one filter can be applied per request");
-            } else {
-                request.entity = "feedback";
-                request.property = "distraction";
-                request.distractions = distraction;
-                return this;
+        public BuildRequest filterProductivity(Integer minProd, Integer maxProd, FilterOperations prodOp) {
+            RequestValidator.validateProductivity(minProd, maxProd, prodOp);
+
+            if (feedbackRequest == null) {
+                feedbackRequest = new FeedbackRequest();
             }
+
+            feedbackRequest.setMinProd(minProd);
+            feedbackRequest.setMaxProd(maxProd);
+            feedbackRequest.setProdOp(prodOp);
+            markFilterApplied();
+            return this;
+        }
+
+        public BuildRequest filterDistractions(List<String> distractions) {
+            if (feedbackRequest == null) {
+                feedbackRequest = new FeedbackRequest();
+            }
+            feedbackRequest.setDistractions(distractions);
+            markFilterApplied();
+            return this;
         }
 
         public FilterRequest build() {
-            if (request.entity == null || request.property == null) {
-                throw new UnsupportedOperationException("Provide a filter for this request");
+            if (!filtersApplied) {
+                throw new UnsupportedOperationException("At least one filter must be applied");
+            } else {
+                request.detailsRequest = detailsRequest;
+                request.timeRequest = timeRequest;
+                request.feedbackRequest = feedbackRequest;
+                return request;
             }
-            return request;
         }
-    }
-
-
-    public String getEntity(){
-        return this.entity;
-    }
-
-    public FilterOperations getOperator() {
-        return this.operator;
-    }
-
-    public String getProperty() {
-        return this.property;
-    }
-    public List<String> getSkills() {
-        return this.skills;
-    }
-
-    public List<String> getDescriptions() {
-        return this.description;
-    }
-
-    public List<Long> getDurations() {
-        return this.durations;
-    }
-
-    public List<LocalDate> getDates() {
-        return this.dates;
-    }
-
-    public List<Integer> getProductivity() {
-        return this.productivity;
-    }
-
-    public List<String> getDistractions() {
-        return this.distractions;
     }
 }
