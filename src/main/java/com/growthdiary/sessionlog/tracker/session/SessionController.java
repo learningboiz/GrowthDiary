523428f22d1e47934b8df5e2d4bc39a3b2addd88
Elijah <elijah.quiazon@gmail.com>
2023-10-26 22:07:20 +1100

    Added ExceptionHandler method to handle validation exceptions thrown by the session

--- src/main/java/com/growthdiary/sessionlog/tracker/session/SessionController.java
+++ src/main/java/com/growthdiary/sessionlog/tracker/session/SessionController.java
@@ -1,11 +1,15 @@
 package com.growthdiary.sessionlog.tracker.session;
 
+import jakarta.validation.Valid;
+import jakarta.validation.ValidationException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@Validated
 public class SessionController {
 
     private final SessionService sessionService;
@@ -16,8 +20,15 @@ public class SessionController {
     }
 
     @PostMapping("/session")
-    public ResponseEntity<Session> session(@RequestBody SessionDTO sessionDTO) {
+    public ResponseEntity<Session> session(@Valid @RequestBody SessionDTO sessionDTO) {
         Session session = sessionService.createSession(sessionDTO);
         return new ResponseEntity<>(session, HttpStatus.CREATED);
+    }
+
+    @ExceptionHandler(ValidationException.class)
+    protected ResponseEntity<String> invalidSessionHandler(ValidationException exception) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(exception.getMessage());
     }
 }
