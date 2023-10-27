e8e8e8a18d99f04abc10a9c796ee1a13bc854364
Elijah <elijah.quiazon@gmail.com>
2023-10-27 23:54:17 +1100

    Updated and pass basic integration tests to ensure that the new workflow works as expected

--- src/test/java/com/growthdiary/sessionlog/sessionhistory/HistoryServiceIntegrationTests.java
+++ src/test/java/com/growthdiary/sessionlog/sessionhistory/HistoryServiceIntegrationTests.java
@@ -1,20 +1,19 @@
 package com.growthdiary.sessionlog.sessionhistory;
 
-import com.growthdiary.sessionlog.history.filters.FilterOperations;
-import com.growthdiary.sessionlog.history.HistoryDTO;
+import com.growthdiary.sessionlog.history.HistoryRepository;
+import com.growthdiary.sessionlog.history.dtos.SortRequestDTO;
+import com.growthdiary.sessionlog.history.historyfilter.DetailsFilter;
+import com.growthdiary.sessionlog.history.historyfilter.FeedbackFilter;
+import com.growthdiary.sessionlog.history.dtos.HistoryViewDTO;
 import com.growthdiary.sessionlog.history.HistoryService;
-import com.growthdiary.sessionlog.history.requests.FilterRequest;
-import com.growthdiary.sessionlog.history.requests.SortRequest;
+import com.growthdiary.sessionlog.history.dtos.FilterRequestDTO;
+import com.growthdiary.sessionlog.history.historyfilter.TimeFilter;
+import com.growthdiary.sessionlog.history.historysort.SortDirection;
 import com.growthdiary.sessionlog.tracker.session.Session;
-import com.growthdiary.sessionlog.tracker.session.SessionRepository;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -25,88 +24,128 @@ import java.util.List;
 public class HistoryServiceIntegrationTests {
 
     @Autowired
-    private SessionRepository sessionRepository;
+    private HistoryRepository historyRepository;
 
     @Autowired
     private HistoryService historyService;
 
-    private Sort sortRequest;
-    private FilterRequest filterRequest;
+    private FilterRequestDTO createFilterRequestDTO() {
 
-    private int totalEntries;
-    private List<String> expectedSkills;
-    private Long expectedMinDuration;
-    private Long expectedMaxDuration;
-    private int expectedMinProd;
-    private int expectedMaxProd;
+        // details filter
+        List<String> skills = Arrays.asList("Spring Boot", "TypeScript");
 
-
-    @BeforeEach
-    public void setRequestValues() {
-        // dummy values
-        totalEntries = 100;
-        expectedSkills = Arrays.asList("Spring Boot", "TypeScript");
-        expectedMinDuration = 30L;
-        expectedMaxDuration = 90L;
-        expectedMinProd = 2;
-        expectedMaxProd = 3;
-        String sortMapping = "time.startDate";
-
-        // build requests
-        sortRequest = SortRequest.sortByDescending(sortMapping);
-        filterRequest = new FilterRequest.BuildRequest()
-                .filterSkill(expectedSkills)
-                .filterDuration(expectedMinDuration, expectedMaxDuration, FilterOperations.BETWEEN)
-                .filterProductivity(expectedMinProd, expectedMaxProd, FilterOperations.BETWEEN)
+        DetailsFilter detailsFilter = new DetailsFilter.BuildFilter()
+                .filterSkill(skills)
                 .build();
+
+        // time filter
+        Long primaryDuration = 30L;
+        Long secondaryDuration = 60L;
+
+        TimeFilter timeFilter = new TimeFilter.BuildFilter()
+                .filterDuration(primaryDuration, secondaryDuration)
+                .build();
+
+        // feedback filter
+        int primaryProductivity = 2;
+        int secondaryProductivity = 4;
+
+        FeedbackFilter feedbackFilter = new FeedbackFilter.BuildFilter()
+                .filterProductivity(primaryProductivity, secondaryProductivity)
+                .build();
+
+        return new FilterRequestDTO(detailsFilter, timeFilter, feedbackFilter);
+    }
+
+    private HistoryViewDTO createHistoryViewDTO() {
+        int pageNum = 0;
+        int pageSize = 10;
+        return new HistoryViewDTO(pageNum, pageSize);
+    }
+
+    private SortRequestDTO createSortRequestDTO() {
+        String sortProperty = "time.startDate";
+        SortDirection sortDirection = SortDirection.DESC;
+        return new SortRequestDTO(sortProperty, sortDirection);
     }
 
     @Test
-    public void testHistoryServiceWithFilter() {
-        int pageNum = 0;
-        int pageSize = 10;
+    public void testHistoryServiceDefaultValues() {
+        Page<Session> sessions = historyService.getDefaultSessions();
 
-        HistoryDTO historyDTO = new HistoryDTO(filterRequest, pageNum, pageSize, sortRequest);
 
-        Page<Session> sessions = historyService.getRequestedSessions(historyDTO);
+        Long expectedSessions = historyRepository.count();
+        Long actualSessions = sessions.getTotalElements();
+
+        assertEquals(expectedSessions, actualSessions);
+        System.out.println("Total sessions: " + expectedSessions);
+    }
+
+    @Test
+    public void testHistoryServiceAllDTOs() {
+        FilterRequestDTO filterRequestDTO = createFilterRequestDTO();
+        HistoryViewDTO historyViewDTO = createHistoryViewDTO();
+        SortRequestDTO sortRequestDTO = createSortRequestDTO();
+
+        Page<Session> sessions = historyService.getRequestedSessions(filterRequestDTO, historyViewDTO, sortRequestDTO);
         for (Session session : sessions) {
+
+            // check that all skill entries are found in the given list
             String actualSkill = session.getDetails().getSkill();
-            Long actualDuration = session.getTime().getDuration();
-            int actualProductivity = session.getFeedback().getProductivity();
+            List<String> expectedSkills = filterRequestDTO.getDetailsFilter().getSkills();
             assertTrue(expectedSkills.contains(actualSkill));
-            assertTrue(actualDuration >= expectedMinDuration && actualDuration <= expectedMaxDuration);
-            assertTrue(actualProductivity >= expectedMinProd && actualProductivity <= expectedMaxProd);
+            System.out.println(session);
         }
     }
 
     @Test
-    public void testHistoryServiceNoFilter() {
-        int pageNum = 0;
-        int pageSize = 10;
-        HistoryDTO historyDTO = new HistoryDTO(null, pageNum, pageSize, sortRequest);
-        Page<Session> sessions = historyService.getRequestedSessions(historyDTO);
+    public void testHistoryServiceOnlyFilters() {
+        FilterRequestDTO filterRequestDTO = createFilterRequestDTO();
 
-        assertEquals(totalEntries, sessions.getTotalElements());
+        Page<Session> sessions = historyService.getRequestedSessions(filterRequestDTO, null, null);
+        for (Session session : sessions) {
+
+            // check that all skill entries are found in the given list
+            String actualSkill = session.getDetails().getSkill();
+            List<String> expectedSkills = filterRequestDTO.getDetailsFilter().getSkills();
+            assertTrue(expectedSkills.contains(actualSkill));
+        }
     }
 
     @Test
-    public void testNegativePageNumAndSize() {
-        int pageNum = -4783;
-        int pageSize = -100;
-        HistoryDTO historyDTO = new HistoryDTO(null, pageNum, pageSize, sortRequest);
+    public void testHistoryServiceNoFilters() {
+        HistoryViewDTO historyViewDTO = createHistoryViewDTO();
+        SortRequestDTO sortRequestDTO = createSortRequestDTO();
 
-        assertThrows(IllegalArgumentException.class, () -> { // exception built into page request
-            Page<Session> sessions = historyService.getRequestedSessions(historyDTO);
-        });
+        Page<Session> sessions = historyService.getRequestedSessions(null, historyViewDTO, sortRequestDTO);
+        Long expectedSessions = historyRepository.count();
+        Long actualSessions = sessions.getTotalElements();
+
+        assertEquals(expectedSessions, actualSessions);
     }
 
     @Test
-    public void testPageSizeGreaterThanSessionCount() {
-        int pageNum = 0;
-        int pageSize = totalEntries + 10;
-        HistoryDTO historyDTO = new HistoryDTO(null, pageNum, pageSize, sortRequest);
-        assertThrows(IllegalArgumentException.class, () -> { // exception built into page request
-            Page<Session> sessions = historyService.getRequestedSessions(historyDTO);
-        });
+    public void testHistoryServiceNoFiltersNoSort() {
+        HistoryViewDTO historyViewDTO = createHistoryViewDTO();
+
+        Page<Session> sessions = historyService.getRequestedSessions(null, historyViewDTO, null);
+        Long expectedSessions = historyRepository.count();
+        Long actualSessions = sessions.getTotalElements();
+
+        assertEquals(expectedSessions, actualSessions);
     }
+
+    @Test
+    public void testHistoryServiceNoFiltersNoView() {
+        SortRequestDTO sortRequestDTO = createSortRequestDTO();
+
+        Page<Session> sessions = historyService.getRequestedSessions(null, null, sortRequestDTO);
+        Long expectedSessions = historyRepository.count();
+        Long actualSessions = sessions.getTotalElements();
+
+        assertEquals(expectedSessions, actualSessions);
+    }
+
+
+
 }
