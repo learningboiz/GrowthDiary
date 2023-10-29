7c9dac567ff1dca0509afdc21e644ba9f9945716
Elijah <elijah.quiazon@gmail.com>
2023-10-29 22:26:13 +1100

    Updated import details

--- src/main/java/com/growthdiary/sessionlog/history/historyfilter/FilterOperations.java
+++ src/main/java/com/growthdiary/sessionlog/history/historyfilter/FilterOperations.java
@@ -1,11 +1,11 @@
 package com.growthdiary.sessionlog.history.historyfilter;
 
-import com.growthdiary.sessionlog.history.dtos.FilterRequestDTO;
+import com.growthdiary.sessionlog.history.requests.FilterRequest;
 
 /**
  * The {@code FilterOperations} enum represents the valid operations that can be used when filtering session attributes.
  * These operations define how values can be compared.
- * @see FilterRequestDTO
+ * @see FilterRequest
  */
 public enum FilterOperations {
     
