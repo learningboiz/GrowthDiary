452d677ea00ac7fd6976476a231d0f145436a5ee
Elijah <elijah.quiazon@gmail.com>
2023-10-29 22:28:08 +1100

    Included ExceptionHandlers to manage the various error handlings, and changed method signature to take in a single DTO

--- src/main/java/com/growthdiary/sessionlog/history/HistoryController.java
+++ src/main/java/com/growthdiary/sessionlog/history/HistoryController.java
@@ -1,13 +1,14 @@
 package com.growthdiary.sessionlog.history;
 
-import com.growthdiary.sessionlog.history.dtos.FilterRequestDTO;
-import com.growthdiary.sessionlog.history.dtos.HistoryViewDTO;
-import com.growthdiary.sessionlog.history.dtos.SortRequestDTO;
+import com.growthdiary.sessionlog.history.requests.SessionHistoryDTO;
 import com.growthdiary.sessionlog.tracker.session.Session;
+import jakarta.validation.ValidationException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,9 +29,28 @@ public class HistoryController {
     }
 
     @GetMapping("/session/history/filter")
-    public ResponseEntity<Page<Session>> requestedSessionHistory(@RequestBody FilterRequestDTO filterRequestDTO,
-                                                                 @RequestBody HistoryViewDTO historyViewDTO,
-                                                                 @RequestBody SortRequestDTO sortRequestDTO) {
-        return new ResponseEntity<>(sessionHistoryService.getRequestedSessions(filterRequestDTO, historyViewDTO, sortRequestDTO), HttpStatus.ACCEPTED);
+    public ResponseEntity<Page<Session>> requestedSessionHistory(@RequestBody SessionHistoryDTO sessionHistoryDTO) {
+        return new ResponseEntity<>(sessionHistoryService.getRequestedSessions(sessionHistoryDTO), HttpStatus.ACCEPTED);
+    }
+
+    @ExceptionHandler(ValidationException.class)
+    protected ResponseEntity<String> handleValidationErrors(ValidationException exception) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(exception.getMessage());
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    protected ResponseEntity<String> handleIllegalArgumentErrors(IllegalArgumentException exception) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(exception.getMessage());
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    protected ResponseEntity<String> handleUnreadableHttpErrors(HttpMessageNotReadableException exception) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(exception.getMessage());
     }
 }
