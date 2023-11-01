96b9e1ebb26f9b32a3e1aae61b9373e8f743fb3a
Elijah <elijah.quiazon@gmail.com>
2023-11-01 23:30:43 +1100

    Modified the repositories being extended: - Initially had SessionRepo extend JPARepo, and HistoryRepo to extend SessionRepo because of the misconception that only SessionRepo was connected to the db - Modified the repos to extend only the functionalities they needed - I.e. Session > CRUD; History > JPA

--- src/main/java/com/growthdiary/sessionlog/history/HistoryRepository.java
+++ src/main/java/com/growthdiary/sessionlog/history/HistoryRepository.java
@@ -2,13 +2,10 @@ package com.growthdiary.sessionlog.history;
 
 import com.growthdiary.sessionlog.tracker.models.Session;
 import com.growthdiary.sessionlog.tracker.SessionRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
-/**
- * Extends SessionRepository to decouple responsibilities
- * Main purpose for this repository is to implement the session history use case
- */
 @Repository
-public interface HistoryRepository extends SessionRepository, JpaSpecificationExecutor<Session> {
+public interface HistoryRepository extends JpaRepository<Session, Long>, JpaSpecificationExecutor<Session> {
 }
--- src/main/java/com/growthdiary/sessionlog/tracker/SessionRepository.java
+++ src/main/java/com/growthdiary/sessionlog/tracker/SessionRepository.java
@@ -2,11 +2,9 @@ package com.growthdiary.sessionlog.tracker;
 
 import com.growthdiary.sessionlog.tracker.models.Session;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-/*
- * Extended JpaRepository as it provides CRUD and Pagination functionality used for the Tracker and History module respectively
- */
 @Repository
-public interface SessionRepository extends JpaRepository<Session, Long> {
+public interface SessionRepository extends CrudRepository<Session, Long> {
 }
