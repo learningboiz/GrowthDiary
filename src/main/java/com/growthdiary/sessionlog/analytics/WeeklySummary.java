c5a4ee3bd86c9cb2c00ff8423cd0171a79252740
Elijah <elijah.quiazon@gmail.com>
2023-11-06 23:46:12 +1100

    added javadocs for the weekly summary projection interface

--- src/main/java/com/growthdiary/sessionlog/analytics/WeeklySummary.java
+++ src/main/java/com/growthdiary/sessionlog/analytics/WeeklySummary.java
@@ -1,5 +1,8 @@
 package com.growthdiary.sessionlog.analytics;
 
+/**
+ * A projection interface to represent a summarised view of the user's weekly session data
+ */
 public interface WeeklySummary {
 
     String getTopSkill();
