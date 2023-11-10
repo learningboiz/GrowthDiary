939c69b7565312315020a4fa7ffc57770d635e2d
Elijah <elijah.quiazon@gmail.com>
2023-11-10 23:48:39 +1100

    Re-organised backend

--- src/main/java/com/growthdiary/sessionlog/analytics/ProductivityAttributes.java
+++
@@ -1,9 +0,0 @@
-package com.growthdiary.sessionlog.analytics;
-
-/**
- * Enums for the supported attributes in creating a productivity chart
- */
-public enum ProductivityAttributes {
-
-    duration, distraction, time
-}
