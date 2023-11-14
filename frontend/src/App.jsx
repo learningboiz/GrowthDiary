484536ed0794d40764f43490624503a63ed769c6
Elijah <elijah.quiazon@gmail.com>
2023-11-14 11:09:40 +1100

    Set up basic routing to session pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    Link
+} from "react-router-dom";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+    return (
+        <Router>
+            <nav>
+                <Link to="/">Home</Link>
+                <Link to="/new-session">New Session</Link>
+                <Link to="/old-session">Old Session</Link>
+            </nav>
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+            <Routes>
+                <Route exact path='/' element={<Home />} />
+                <Route path='/new-session' element={<NewSession />} />
+                <Route path='/old-session' element={<OldSession />} />
+            </Routes>
+        </Router>
+    );
 }
 
-export default App
+function Home() {
+    return <h2>Welcome back</h2>
+}
+
+function NewSession() {
+    return <h2>Start a new session</h2>
+}
+
+function OldSession() {
+    return <h2>Log a completed session</h2>
+}
