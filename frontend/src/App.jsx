dde4652e979c20562a9649b498af0b6025c295aa
Elijah <elijah.quiazon@gmail.com>
2023-11-21 00:21:28 +1100

    Added navbar and routes components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,14 @@
 import './App.css'
-import MainSessionPage from './tracker/pages/MainSessionPage.jsx'
-import {
-    BrowserRouter as Router,
-    Routes,
-    Route,
-    Link
-} from "react-router-dom";
-import SessionPageButton from "./tracker/SessionPageButton.jsx";
+import MainRoutes from "./mainroutes.jsx";
+import NavigationBar from "./components/navigationbar.jsx";
+
 
 export default function App() {
     return (
-        <Router>
-            <nav>
-                <Link to="/">Home</Link>
-                <Link to="/session">Start Session</Link>
-            </nav>
-
-            <Routes>
-                <Route exact path='/' element={<Home />} />
-                <Route path='/session' element={<MainSessionPage />} />
-            </Routes>
-        </Router>
-    );
-}
-
-function Home() {
-    return (
         <>
-            <h2>Welcome back</h2>
-            <SessionPageButton />
+            <NavigationBar />
+            <MainRoutes />
         </>
-
     )
 }
+
