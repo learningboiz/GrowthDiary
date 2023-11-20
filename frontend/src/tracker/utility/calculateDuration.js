a25db445344ab50908944ef206eb2a86725dbbb0
Elijah <elijah.quiazon@gmail.com>
2023-11-20 22:58:41 +1100

    added configurations to react-testing-library

--- frontend/src/tracker/utility/calculateDuration.js
+++
@@ -1,6 +0,0 @@
-export function calculateDuration(startPeriod, endPeriod) {
-    const startDate = new Date(startPeriod);
-    const endDate = new Date(endPeriod);
-    const differenceInMilliseconds = endDate - startDate;
-    return Math.round(differenceInMilliseconds / 60000);
-}
