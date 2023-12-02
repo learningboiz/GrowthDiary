11fbba71ea4d4d515c9fbfbc240e3dd91e8c81b4
Elijah <elijah.quiazon@gmail.com>
2023-12-03 00:39:08 +1100

    added new route for the creation of real-time sessions

--- frontend/src/routes/ApplicationRoutes.jsx
+++ frontend/src/routes/ApplicationRoutes.jsx
@@ -1,14 +1,16 @@
 import {Route, Routes} from 'react-router-dom'
 import HomePage from "../pages/homepage.jsx";
 import AboutPage from "../pages/aboutpage.jsx";
-import SessionPage from "../pages/sessionpage.jsx";
+import SessionMainPage from "../pages/tracker/SessionMainPage.jsx";
+import FormDetails from "../pages/tracker/FormDetails.jsx";
 
 export default function ApplicationRoutes() {
     return (
         <Routes>
             <Route path="/" element={<HomePage />}></Route>
             <Route path="/about" element={<AboutPage />}></Route>
-            <Route path="/session" element={<SessionPage />}></Route>
+            <Route path="/session" element={<SessionMainPage />}></Route>
+            <Route path="/session/new" element={<FormDetails />}></Route>
         </Routes>
     )
 }
