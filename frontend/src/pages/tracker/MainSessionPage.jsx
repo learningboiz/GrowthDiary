a1731bca63bf1ac32e9987a4db37f6d8a16f8e6d
Elijah <elijah.quiazon@gmail.com>
2023-12-03 15:59:34 +1100

    added resetformvalues method to be passed through the context provider

---
+++ frontend/src/pages/tracker/MainSessionPage.jsx
@@ -0,0 +1,10 @@
+import StartNewSession from "./StartNewSession.jsx";
+
+export default function SessionIndex() {
+    return (
+        <>
+            <h2>Track your learning session in real-time!</h2>
+            <StartNewSession />
+        </>
+    )
+}
--- frontend/src/pages/tracker/SessionContext.jsx
+++ frontend/src/pages/tracker/SessionContext.jsx
@@ -1,9 +1,18 @@
 import {createContext, useState} from 'react';
 
+/**
+ * Context to manage the state of a session form
+ */
 export const SessionContext = createContext();
 
+/**
+ * Provides methods to view and update existing form data in the context, and reset values to default
+ * Reset values method created to allow form to re-render after each completion since components can only re-render after changes to state
+ *
+ * Context provider wrapped around app entry point to ensure context can be used in every route
+ */
 export const SessionContextProvider = ({ children }) => {
-    const sessionFormTemplate = {
+    const initialFormValues = {
         topic: "",
         description: "",
         startPeriod: {},
@@ -12,10 +21,14 @@ export const SessionContextProvider = ({ children }) => {
         productivity: "",
     };
 
-    const [sessionForm, setSessionForm] = useState(sessionFormTemplate);
+    const [sessionForm, setSessionForm] = useState(initialFormValues);
+
+    const resetFormValues = () => {
+        setSessionForm(initialFormValues)
+    }
 
     return (
-        <SessionContext.Provider value={{sessionForm, setSessionForm}}>
+        <SessionContext.Provider value={{sessionForm, setSessionForm, resetFormValues}}>
             {children}
         </SessionContext.Provider>
     );
