2ee7daff0f1893c47c63104133f9f91169fa6562
Elijah <elijah.quiazon@gmail.com>
2023-12-03 16:00:11 +1100

    utilised resetformvalues to ensure context is resetted and allows the form to be re-rendered after initial submission

--- frontend/src/pages/tracker/FormDetails.jsx
+++ frontend/src/pages/tracker/FormDetails.jsx
@@ -3,11 +3,16 @@ import FormTime from "./FormTime.jsx";
 import {SessionContext} from "./SessionContext.jsx";
 
 export default function FormDetails() {
-    const { setSessionForm } = useContext(SessionContext)
+    const { setSessionForm, resetFormValues } = useContext(SessionContext)
     const [sectionCompleted, setSectionCompleted] = useState(false);
 
+    if (sectionCompleted) {
+        return <FormTime />
+    }
 
     const saveData = (e) => {
+        resetFormValues();
+
         setSessionForm((prevSessionForm) => ({
             ...prevSessionForm,
             [e.target.name]: e.target.value
@@ -17,36 +22,32 @@ export default function FormDetails() {
     const submitForm = (e) => {
         e.preventDefault();
         setSectionCompleted(true)
+
     }
 
     return (
         <>
-            {!sectionCompleted &&
-                <>
-                    <h2>Step 1</h2>
-                    <p>Fill in the details below</p>
-                    <form onSubmit={submitForm}>
-                        <label>
-                            Topic
-                            <input
-                                type="text"
-                                name="topic"
-                                onChange={saveData}
-                            />
-                        </label>
-                        <label>
-                            Description
-                            <input
-                                type="text"
-                                name="description"
-                                onChange={saveData}
-                            />
-                        </label>
-                        <button type="submit">Next</button>
-                    </form>
-                </>
-            }
-            {sectionCompleted && <FormTime />}
+            <h2>Step 1</h2>
+            <p>Fill in the details below</p>
+            <form onSubmit={submitForm}>
+                <label>
+                    Topic
+                    <input
+                        type="text"
+                        name="topic"
+                        onChange={saveData}
+                    />
+                </label>
+                <label>
+                    Description
+                    <input
+                        type="text"
+                        name="description"
+                        onChange={saveData}
+                    />
+                </label>
+                <button type="submit">Next</button>
+            </form>
         </>
     )
 }
