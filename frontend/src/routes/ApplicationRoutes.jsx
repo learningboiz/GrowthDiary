46602927d93fbd955b2fd26525e1baea2768adea
Elijah <elijah.quiazon@gmail.com>
2023-12-06 12:59:56 +1100

    Added routing for new and old session tracker

--- frontend/src/routes/ApplicationRoutes.jsx
+++ frontend/src/routes/ApplicationRoutes.jsx
@@ -1,8 +1,10 @@
 import {Route, Routes} from 'react-router-dom'
 import HomePage from "../pages/homepage.jsx";
 import AboutPage from "../pages/aboutpage.jsx";
-import MainSessionPage from "../pages/tracker/MainSessionPage.jsx";
-import FormDetails from "../pages/tracker/FormDetails.jsx";
+import MainSessionPage from "../tracker/views/MainSessionPage.jsx";
+import DetailsForm from "../tracker/forms/DetailsForm.jsx";
+import NewSessionPage from "../tracker/views/NewSessionPage.jsx";
+import OldSessionPage from "../tracker/views/OldSessionPage.jsx";
 
 export default function ApplicationRoutes() {
     return (
@@ -10,7 +12,8 @@ export default function ApplicationRoutes() {
             <Route path="/" element={<HomePage />}></Route>
             <Route path="/about" element={<AboutPage />}></Route>
             <Route path="/session" element={<MainSessionPage />}></Route>
-            <Route path="/session/new" element={<FormDetails />}></Route>
+            <Route path="/session/new" element={<NewSessionPage />}></Route>
+            <Route path="/session/old" element={<OldSessionPage />}></Route>
         </Routes>
     )
 }
