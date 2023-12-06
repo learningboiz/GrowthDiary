8b31d0326a2abcaf0c75e77ec5ef893a7d315b21
Elijah <elijah.quiazon@gmail.com>
2023-12-06 13:02:31 +1100

    Added list of obstacles for drop down menu

---
+++ frontend/src/tracker/utility/obstacleList.js
@@ -0,0 +1,16 @@
+export const obstacleList = [
+    "Overthinking",
+    "Perfectionism",
+    "Burnout",
+    "Social media",
+    "Gaming",
+    "Streaming services",
+    "Talking/texting",
+    "Lack of direction",
+    "Lack of interest",
+    "Lack of peer support",
+    "Noisy surroundings",
+    "Limited time",
+    "Over-reliance on tutorials",
+    "Ineffective learning resources",
+];
