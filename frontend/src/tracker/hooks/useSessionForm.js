0428b1e0a27bb022dce78c186bf75d4701fce297
Elijah <elijah.quiazon@gmail.com>
2023-12-09 11:46:59 +1100

    Added JSDocs for the useSessionForm hook methods

--- frontend/src/tracker/hooks/useSessionForm.js
+++ frontend/src/tracker/hooks/useSessionForm.js
@@ -1,9 +1,16 @@
 import {useContext} from "react";
 import {SessionContext} from "../SessionContext.jsx";
 
+/**
+ * Provides methods to handle the saving of user form input and progression through different form components
+ * @returns {{saveFormProgress: saveFormProgress, saveTimeInput: saveTimeInput, saveInput: saveInput}}
+ */
 export function useSessionForm(stepUpdater) {
-    const { sessionForm, setSessionForm } = useContext(SessionContext);
+    const { setSessionForm } = useContext(SessionContext);
 
+    /**
+     * Registers data from user input field into the respective sessionForm attribute
+     */
     const saveInput = (e) => {
         setSessionForm((prevSessionForm) => ({
             ...prevSessionForm,
@@ -11,6 +18,10 @@ export function useSessionForm(stepUpdater) {
         }))
     }
 
+    /**
+     * Registers time data by creating a data based on user input
+     * Created a separate method since date creation is required
+     */
     const saveTimeInput = (e) => {
         setSessionForm((prevSessionForm) => ({
             ...prevSessionForm,
@@ -18,6 +29,9 @@ export function useSessionForm(stepUpdater) {
         }))
     }
 
+    /**
+     * Updates the form stepper to allow the correct order of form component rendering
+     */
     const saveFormProgress = (e) => {
         e.preventDefault();
         stepUpdater();
