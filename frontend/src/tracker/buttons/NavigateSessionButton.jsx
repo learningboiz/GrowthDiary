953402ff531e2265c7a692a0f281c085497de536
Elijah <elijah.quiazon@gmail.com>
2023-12-09 12:14:05 +1100

    modified the styles to the session button

--- frontend/src/tracker/buttons/NavigateSessionButton.jsx
+++ frontend/src/tracker/buttons/NavigateSessionButton.jsx
@@ -1,7 +1,7 @@
 import {useNavigate} from "react-router-dom";
 import {useContext} from "react";
 import {SessionContext} from "../SessionContext.jsx";
-import styles from "../../styles/tracker/navigateSessionButton.module.css";
+import styles from "../../styles/tracker/sessionPage.module.css";
 
 export default function NavigateSessionButton({newSession}) {
 
@@ -18,6 +18,6 @@ export default function NavigateSessionButton({newSession}) {
         navigate(path, {replace: true})
     }
 
-    return <button className={styles.navigateSessionButton}
+    return <button className={styles.sessionPageButton}
         onClick={startSession}>{buttonText}</button>
 }
