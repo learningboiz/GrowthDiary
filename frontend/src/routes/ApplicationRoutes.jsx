c6e0064f92ae872724d412fb6edc6a7a5ecc67f1
Elijah <elijah.quiazon@gmail.com>
2023-12-09 12:10:19 +1100

    Modified component to be routed to trhough /session

--- frontend/src/routes/ApplicationRoutes.jsx
+++ frontend/src/routes/ApplicationRoutes.jsx
@@ -1,7 +1,7 @@
 import {Route, Routes} from 'react-router-dom'
 import HomePage from "../pages/homepage.jsx";
 import AboutPage from "../pages/aboutpage.jsx";
-import MainSessionPage from "../tracker/views/MainSessionPage.jsx";
+import BeforeSessionView from "../tracker/views/BeforeSessionView.jsx";
 import DetailsForm from "../tracker/forms/DetailsForm.jsx";
 import NewSessionPage from "../tracker/views/NewSessionPage.jsx";
 import OldSessionPage from "../tracker/views/OldSessionPage.jsx";
@@ -11,7 +11,7 @@ export default function ApplicationRoutes() {
         <Routes>
             <Route path="/" element={<HomePage />}></Route>
             <Route path="/about" element={<AboutPage />}></Route>
-            <Route path="/session" element={<MainSessionPage />}></Route>
+            <Route path="/session" element={<BeforeSessionView />}></Route>
             <Route path="/session/new" element={<NewSessionPage />}></Route>
             <Route path="/session/old" element={<OldSessionPage />}></Route>
         </Routes>
