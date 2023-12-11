ad87fc1199bcf4b99166d48f5c36494e9d097b4f
Elijah <elijah.quiazon@gmail.com>
2023-12-11 12:56:00 +1100

    Refactored existing forms to incorporate the react hook form and changes to the underlying session form object

--- frontend/src/tracker/forms/FeedbackForm.jsx
+++ frontend/src/tracker/forms/FeedbackForm.jsx
@@ -1,26 +1,56 @@
-import {useSessionForm} from "../hooks/useSessionForm.js";
 import {obstacleList} from "../utility/obstacleList.js";
 import styles from "../../styles/tracker/sessionForm.module.css"
+import {useContext} from "react";
+import {SessionContext} from "../SessionContext.jsx";
+import {useForm} from "react-hook-form";
+import {ErrorMessage} from "@hookform/error-message";
 
 export default function FeedbackForm({stepUpdater}) {
-    const { saveInput, saveFormProgress } = useSessionForm(stepUpdater);
+    const {sessionForm, setSessionForm} = useContext(SessionContext);
+    const { register,
+        handleSubmit,
+        formState: {errors},
+    } = useForm();
+
+    const onSubmit = (data) => {
+        setSessionForm((prevSessionForm) => ({
+            ...prevSessionForm,
+            obstacle: data.obstacle,
+            productivity: data.productivity,
+        }))
+        stepUpdater();
+
+        console.log(sessionForm)
+    }
 
     return (
         <div className={styles.sessionForm}>
             <h2>Feedback</h2>
             <h3>How did the session go?</h3>
-            <form onSubmit={saveFormProgress}>
+            <form onSubmit={handleSubmit(onSubmit)}>
                 <label>
                     Key Obstacle
-                    <select name="obstacle"
-                            required={true}
-                            onChange={saveInput}>
+                    <select {...register("obstacle", {
+                        required: "Please pick an obstacle"
+                    })}
+                        defaultValue=""
+                    >
+                        <option value="" disabled>
+                            Please pick an option below
+                        </option>
                         {obstacleList.map((obstacle) => (
                             <option key={obstacle}>{obstacle}</option>
                             )
                         )}
                     </select>
                 </label>
+                <div className={styles.sessionFormError}>
+                    <ErrorMessage
+                        errors={errors}
+                        name="obstacle"
+                        render={({ message }) => <p>{message}</p>}
+                    />
+                </div>
                 <label>
                     Productivity
                     <input
@@ -28,11 +58,19 @@ export default function FeedbackForm({stepUpdater}) {
                         min="1"
                         max="5"
                         step="1"
-                        name="productivity"
-                        required={true}
-                        onChange={saveInput}
+                        defaultValue="3"
+                        {...register("productivity", {
+                            required: "Please provide a productivity rating",
+                        })}
                     />
                 </label>
+                <div className={styles.sessionFormError}>
+                    <ErrorMessage
+                        errors={errors}
+                        name="productivity"
+                        render={({ message }) => <p>{message}</p>}
+                    />
+                </div>
                 <button type="submit">Complete</button>
             </form>
         </div>
