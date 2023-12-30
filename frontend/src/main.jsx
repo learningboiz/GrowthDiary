ad379b18e09b71abe03b293fe2b7f95bc68a56a1
Elijah <elijah.quiazon@gmail.com>
2023-12-30 18:52:03 +1100

    updates to file packing and names

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx';
 import './index.css'
 import { BrowserRouter } from "react-router-dom";
-import {SessionContextProvider} from "./tracker/SessionContext.jsx";
+import {SessionContextProvider} from "./features/sessiontracker/SessionContext.jsx";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
