53181b88cc26e27feeff043908558fd423ccd0d3
Elijah <elijah.quiazon@gmail.com>
2023-12-30 19:00:19 +1100

    Changed text colour to indigo to match app's colour theme

--- frontend/src/features/sessionhistory/components/PageSizeToggle.jsx
+++ frontend/src/features/sessionhistory/components/PageSizeToggle.jsx
@@ -14,11 +14,11 @@ export default function PageSizeToggle({setHistoryDTO}) {
 
     return (
         <div className="inline-flex items-center gap-x-2">
-            <p className="text-sm text-gray-600">
-                Items per page:
+            <p className="text-sm text-indigo-500">
+                Items per page
             </p>
             <div className="max-w-sm space-y-3">
-                <select className="py-2 px-3 pe-9 block w-full bg-indigo-500 border-gray-200 rounded-lg text-sm font-medium text-neutral-50 focus:border-blue-500 focus:ring-blue-500"
+                <select className="py-2 px-3 pe-2 block w-full bg-indigo-500 border-gray-200 rounded-lg text-sm font-medium text-neutral-50 focus:border-blue-500 focus:ring-blue-500"
                         onChange={handlePageSizeToggle}>
                     <option value={10}>10</option>
                     <option value={15}>15</option>
--- frontend/src/features/sessionhistory/components/PaginationToggle.jsx
+++ frontend/src/features/sessionhistory/components/PaginationToggle.jsx
@@ -72,19 +72,19 @@ export default function PaginationToggle({totalPages, setHistoryDTO}) {
             <button
                 type="button"
                 onClick={handlePrevPage}
-                className="min-h-[38px] min-w-[38px] py-2 px-2.5 inline-flex justify-center items-center gap-x-2 text-sm rounded-lg text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100">
+                className="min-h-[38px] min-w-[38px] py-2 px-2.5 inline-flex justify-center items-center gap-x-2 text-sm rounded-lg text-indigo-600 hover:bg-gray-100 focus:outline-none focus:bg-gray-100">
                 <svg className="flex-shrink-0 w-3.5 h-3.5" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="m15 18-6-6 6-6"/></svg>
                 <span aria-hidden="true" className="sr-only">Previous</span>
             </button>
             <div className="flex items-center gap-x-1">
-                <span className="min-h-[38px] min-w-[38px] flex justify-center items-center border border-indigo-300 text-indigo-600 py-2 px-3 text-sm rounded-lg focus:outline-none focus:bg-gray-50">{pageUserIsOn}</span>
-                <span className="min-h-[38px] flex justify-center items-center text-gray-500 py-2 px-1.5 text-sm">of</span>
-                <span className="min-h-[38px] flex justify-center items-center text-gray-500 py-2 px-1.5 text-sm">{totalPages}</span>
+                <span className="min-h-[38px] min-w-[38px] flex justify-center items-center border border-indigo-600 text-indigo-500 py-2 px-3 text-sm rounded-lg focus:outline-none focus:bg-gray-50">{pageUserIsOn}</span>
+                <span className="min-h-[38px] flex justify-center items-center text-indigo-500 py-2 px-1.5 text-sm">of</span>
+                <span className="min-h-[38px] flex justify-center items-center text-indigo-500 py-2 px-1.5 text-sm">{totalPages}</span>
             </div>
             <button
                 type="button"
                 onClick={handleNextPage}
-                className="min-h-[38px] min-w-[38px] py-2 px-2.5 inline-flex justify-center items-center gap-x-2 text-sm rounded-lg text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100">
+                className="min-h-[38px] min-w-[38px] py-2 px-2.5 inline-flex justify-center items-center gap-x-2 text-sm rounded-lg text-indigo-600 hover:bg-gray-100 focus:outline-none focus:bg-gray-100">
                 <span aria-hidden="true" className="sr-only">Next</span>
                 <svg className="flex-shrink-0 w-3.5 h-3.5" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="m9 18 6-6-6-6"/></svg>
             </button>
