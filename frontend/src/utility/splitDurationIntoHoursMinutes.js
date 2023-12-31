e475c8aabecfbec2ef1e09eb6fa9fc6058222762
Elijah <elijah.quiazon@gmail.com>
2023-12-31 18:08:20 +1100

    moved utils to common utils and updated naming for better readability

--- frontend/src/utility/splitDurationIntoHoursMinutes.js
+++ frontend/src/utility/splitDurationIntoHoursMinutes.js
@@ -1,4 +1,4 @@
-export function getHoursAndMinutes(duration) {
+export function splitDurationIntoHoursMinutes(duration) {
     const hourAndMinute = duration / 60;
     const hours = Math.floor(hourAndMinute);
     const minutes = Math.round((hourAndMinute - hours) * 60);
--- frontend/src/utility/splitPeriodIntoDateTime.js
+++ frontend/src/utility/splitPeriodIntoDateTime.js
@@ -1,4 +1,11 @@
+/*
+ * Parses a time period into an array of date and time
+ * Useful within JS since initializing new Date() produces a period rather than a specific date
+ */
 export function splitPeriodIntoDateTime(timePeriod) {
+    // TODO might want to handle the iso string change in this util
+
+
     const parsedTimePeriod = timePeriod.split('T');
     const date = parsedTimePeriod[0];
     const time = parsedTimePeriod[1].replace('Z', '');
