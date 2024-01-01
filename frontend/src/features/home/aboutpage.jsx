75daf9ad88dd94fdcfa44b013a7f4d89e160f49e
Elijah <elijah.quiazon@gmail.com>
2024-01-01 13:13:39 +1100

    set-up home view with button navs to other features

--- frontend/src/features/home/aboutpage.jsx
+++
@@ -1,3 +0,0 @@
-export default function AboutPage() {
-    return <h2>About this app</h2>
-}
--- frontend/src/features/home/homepage.jsx
+++ frontend/src/features/home/homepage.jsx
@@ -1,7 +1,33 @@
+import {useNavigate} from "react-router-dom";
+import OutlineButton from "../../components/buttons/OutlineButton.jsx";
+
 export default function HomePage() {
+
+    const navigate = useNavigate();
+
+    const handleSessionNav = (e) => {
+        e.preventDefault();
+        navigate("/session", {replace: true})
+    }
+
+    const handleHistoryNav = (e) => {
+        e.preventDefault();
+        navigate("/history", {replace: true})
+    }
+
+    const handleAnalyticsNav = (e) => {
+        e.preventDefault();
+        navigate("/analytics", {replace: true})
+    }
+
     return (
-        <>
-            <h2>Welcome</h2>
-        </>
+        <div className="py-8 px-4 mx-auto max-w-2xl lg:py-16">
+            <h2 className="text-2xl font-bold mb-4 text-indigo-600">Welcome back</h2>
+            <div className="flex flex-col gap-4 w-full">
+                <OutlineButton handleOnClick={handleSessionNav} buttonText={"Start a session"} />
+                <OutlineButton handleOnClick={handleHistoryNav} buttonText={"Check session history"} />
+                <OutlineButton handleOnClick={handleAnalyticsNav} buttonText={"View analytics"} />
+            </div>
+        </div>
     )
 }
