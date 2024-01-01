d104913c6ddcf4bba5554a81759e2b7826f94f4a
Elijah <elijah.quiazon@gmail.com>
2024-01-01 13:15:28 +1100

    modified descriptions to include numerical value

--- frontend/src/features/sessiontracker/utility/productivityDescription.js
+++ frontend/src/features/sessiontracker/utility/productivityDescription.js
@@ -1,7 +1,7 @@
 export const productivityDescriptions = {
-    1: "Very Low",
-    2: "Low",
-    3: "Moderate",
-    4: "High",
-    5: "Very High",
+    1: "[1] Very Low",
+    2: "[2] Low",
+    3: "[3] Moderate",
+    4: "[4] High",
+    5: "[5] Very High",
 };
