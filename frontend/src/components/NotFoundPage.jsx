fdaffa6f52060a2c5b9b10fb4581d84b9990ef95
Elijah <elijah.quiazon@gmail.com>
2024-01-03 00:02:34 +1100

    created error 404 page to render whenever user navigates to a non-valid route

---
+++ frontend/src/components/NotFoundPage.jsx
@@ -0,0 +1,10 @@
+export default function NotFoundPage() {
+    return (
+        <div className="text-center py-10 px-4 sm:px-6 lg:px-8">
+            <h1 className="block text-7xl font-bold text-indigo-700 sm:text-9xl">404</h1>
+            <h1 className="block text-2xl font-bold text-white"></h1>
+            <p className="mt-3 text-indigo-600">Oops, something went wrong.</p>
+            <p className="text-indigo-600">Sorry, we couldn't find your page.</p>
+        </div>
+    )
+}
--- frontend/src/routes/ApplicationRoutes.jsx
+++ frontend/src/routes/ApplicationRoutes.jsx
@@ -1,58 +1,22 @@
 import {Route, Routes} from 'react-router-dom'
-import Signup from "../features/authentication/Signup.jsx";
-import Login from "../features/authentication/Login.jsx"
+import NotFoundPage from "../components/NotFoundPage.jsx";
 import HomePage from "../features/home/homepage.jsx";
 import BeforeSessionView from "../features/sessiontracker/views/BeforeSessionView.jsx";
 import NewSessionPage from "../features/sessiontracker/views/NewSessionPage.jsx";
 import OldSessionPage from "../features/sessiontracker/views/OldSessionPage.jsx";
 import HistoryView from "../features/sessionhistory/HistoryView.jsx";
 import AnalyticsView from "../features/sessionanalytics/AnalyticsView.jsx";
-import ProtectedRoute from "./ProtectedRoute.jsx";
 export default function ApplicationRoutes() {
-
 
     return (
         <Routes>
-            <Route path="*" element={<h2>Hello</h2>}></Route>
-
-            <Route path="/" element={<Login />}></Route>
-            <Route path="/signup" element={<Signup />}></Route>
-
-            <Route path="/home" element={
-                <ProtectedRoute>
-                    <HomePage />
-                </ProtectedRoute>
-                }></Route>
-
-            <Route path="/session" element={
-                <ProtectedRoute>
-                    <BeforeSessionView />
-                </ProtectedRoute>
-                }></Route>
-
-            <Route path="/session/new" element={
-                <ProtectedRoute>
-                    <NewSessionPage />
-                </ProtectedRoute>
-                }></Route>
-
-            <Route path="/session/old" element={
-                <ProtectedRoute>
-                    <OldSessionPage />
-                </ProtectedRoute>
-                }></Route>
-
-            <Route path="/history" element={
-                <ProtectedRoute>
-                    <HistoryView />
-                </ProtectedRoute>
-                }></Route>
-
-            <Route path="/analytics" element={
-                <ProtectedRoute>
-                    <AnalyticsView />
-                </ProtectedRoute>
-                }></Route>
+            <Route path="/" element={<HomePage />}></Route>
+            <Route path="/session" element={<BeforeSessionView />}></Route>
+            <Route path="/session/new" element={<NewSessionPage />}></Route>
+            <Route path="/session/old" element={<OldSessionPage />}></Route>
+            <Route path="/history" element={<HistoryView />}></Route>
+            <Route path="/analytics" element={ <AnalyticsView />}></Route>
+            <Route path="*" element={<NotFoundPage />}></Route>
         </Routes>
     )
 }
