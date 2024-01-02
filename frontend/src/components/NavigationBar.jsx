4ab33a52b242b166571451e1d7de65254cafe4c7
Elijah <elijah.quiazon@gmail.com>
2024-01-03 00:03:30 +1100

    Updated link of home to /

--- frontend/src/components/NavigationBar.jsx
+++ frontend/src/components/NavigationBar.jsx
@@ -1,20 +1,13 @@
 import {Link} from "react-router-dom";
-import { useAuth } from "../features/authentication/AuthContext.jsx";
 
 export default function NavigationBar() {
-
-    const { user } = useAuth();
-
-    if (!user) {
-        return null;
-    }
 
     return (
         <nav className="flex justify-end p-4">
             <ul className="flex space-x-4">
                 <li>
                     <Link className="text-indigo-500 hover:text-indigo-700 hover:font-medium transition duration-300"
-                          to="home">Home</Link>
+                          to="/">Home</Link>
                 </li>
                 <li>
                     <Link className="text-indigo-500 hover:text-indigo-700 hover:font-medium transition duration-300"
